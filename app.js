fddf74da996dc9243898c815bb4b491bae411ed1
rachana754 <43152506+rachana754@users.noreply.github.com>
2019-03-22 01:49:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,14 @@ var express = require('express');
 var app = express();
 app.set('view engine','ejs');
 app.use('/assets',express.static('assets'));
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+app.use(session({resave: true,secret: 'secretToken', saveUninitialized:true}));
 var catalogController = require('./controller/catalogController');
-//  routes defining
-app.use('/', catalogController);
-app.use('/myItems', catalogController);
-app.use('/categories/item/:itemCode',catalogController);
-app.use('/categories/:categoryName',catalogController);
-app.use('/categories',catalogController);
-app.use('/contact', catalogController);
-app.use('/about', catalogController);
-app.use('/feedback',catalogController);
+var profileController = require('./controller/profileController');
+app.use(catalogController);
+app.use(profileController);
 app.listen(8080,function(){
     console.log('app started')
     console.log('listening on port 8080')
